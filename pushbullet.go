439e22cc5a2c088ce1203f9d19e1ce0fe0aa23c6
Damian Gryski <damian@gryski.com>
2014-11-22 17:06:28 +0100

    Upgrade to API v2
    This is a breaking change.
    
    The API v1 endpoints are no longer reliable.

--- pushbullet.go
+++ pushbullet.go
@@ -1,26 +1,28 @@
+// Package pushbullet provides simple access to the v2 API of http://pushbullet.com.
 /*
-Package pushbullet provides simple access to the API of http://pushbullet.com.
 
 Example client:
 	pb := pushbullet.New("YOUR_API_KEY")
 	devices, err := pb.Devices()
 	...
-	err = pb.PushNote(devices[0].Id, "Hello!", "Hi from go-pushbullet!")
+	err = pb.PushNote(devices[0].Iden, "Hello!", "Hi from go-pushbullet!")
+
+The API is document at https://docs.pushbullet.com/http/ .  At the moment, it only supports querying devices and sending notifications.
+
 */
 package pushbullet
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"strconv"
-	"strings"
 )
 
-const HOST = "https://api.pushbullet.com/api"
+var Endpoint = "https://api.pushbullet.com/v2"
 
 // A Client connects to PushBullet with an API Key.
 type Client struct {
@@ -38,18 +40,35 @@ func NewWithClient(apikey string, client *http.Client) *Client {
 	return &Client{apikey, client}
 }
 
-// A Device represents an Android Device as reported by PushBullet.
+// A Device is a PushBullet device
 type Device struct {
-	Id        int
-	OwnerName string `json:"owner_name"`
-	Extras    struct {
-		Manufacturer   string
-		Model          string
-		AndroidVersion string `json:"android_version"`
-		SdkVersion     string `json:"sdk_version"`
-		AppVersion     int    `json:"app_version"`
-		Nickname       string
-	}
+	Iden         string  `json:"iden"`
+	PushToken    string  `json:"push_token"`
+	AppVersion   int     `json:"app_version"`
+	Fingerprint  string  `json:"fingerprint"`
+	Active       bool    `json:"active"`
+	Nickname     string  `json:"nickname"`
+	Manufacturer string  `json:"manufacturer"`
+	Type         string  `json:"type"`
+	Created      float32 `json:"created"`
+	Modified     float32 `json:"modified"`
+	Model        string  `json:"model"`
+	Pushable     bool    `json:"pushable"`
+}
+
+// ErrResponse is an error returned by the PushBullet API
+type ErrResponse struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
+	Cat     string `json:"cat"`
+}
+
+func (e *ErrResponse) Error() string {
+	return e.Message
+}
+
+type errorResponse struct {
+	ErrResponse `json:"error"`
 }
 
 type deviceResponse struct {
@@ -57,8 +76,8 @@ type deviceResponse struct {
 	SharedDevices []*Device `json:"shared_devices"`
 }
 
-func (c *Client) buildRequest(endpoint string, data url.Values) *http.Request {
-	r, err := http.NewRequest("GET", HOST+endpoint, nil)
+func (c *Client) buildRequest(object string, data interface{}) *http.Request {
+	r, err := http.NewRequest("GET", Endpoint+object, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -69,8 +88,11 @@ func (c *Client) buildRequest(endpoint string, data url.Values) *http.Request {
 
 	if data != nil {
 		r.Method = "POST"
-		r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-		r.Body = ioutil.NopCloser(strings.NewReader(data.Encode()))
+		r.Header.Set("Content-Type", "application/json")
+		var b bytes.Buffer
+		enc := json.NewEncoder(&b)
+		enc.Encode(data)
+		r.Body = ioutil.NopCloser(&b)
 	}
 
 	return r
@@ -83,10 +105,15 @@ func (c *Client) Devices() ([]*Device, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer func() {
-		resp.Body.Close()
-	}()
+	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
+		var errjson errorResponse
+		dec := json.NewDecoder(resp.Body)
+		err = dec.Decode(&errjson)
+		if err == nil {
+			return nil, &errjson.ErrResponse
+		}
+
 		return nil, errors.New(resp.Status)
 	}
 
@@ -101,61 +128,101 @@ func (c *Client) Devices() ([]*Device, error) {
 	return devices, nil
 }
 
-// Push pushes the data to a specific device registered with PushBullet.
-func (c *Client) Push(deviceId int, data url.Values) error {
-	data.Set("device_id", strconv.Itoa(deviceId))
+// Push pushes the data to a specific device registered with PushBullet.  The
+// 'data' parameter is marshaled to JSON and sent as the request body.  Most
+// users should call one of PusNote, PushLink, PushAddress, or PushList.
+func (c *Client) Push(data interface{}) error {
 	req := c.buildRequest("/pushes", data)
 	resp, err := c.Client.Do(req)
 	if err != nil {
 		return err
 	}
-	defer func() {
-		resp.Body.Close()
-	}()
+	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
+		var errResponse errorResponse
+		dec := json.NewDecoder(resp.Body)
+		err = dec.Decode(&errResponse)
+		if err == nil {
+			return &errResponse.ErrResponse
+		}
+
 		return errors.New(resp.Status)
 	}
 
 	return nil
 }
 
+type Note struct {
+	Iden  string `json:"device_iden"`
+	Type  string `json:"type"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
 // PushNote pushes a note with title and body to a specific PushBullet device.
-func (c *Client) PushNote(deviceId int, title, body string) error {
-	data := url.Values{
-		"type":  {"note"},
-		"title": {title},
-		"body":  {body},
+func (c *Client) PushNote(iden string, title, body string) error {
+	data := Note{
+		Iden:  iden,
+		Type:  "note",
+		Title: title,
+		Body:  body,
 	}
-	return c.Push(deviceId, data)
+	return c.Push(data)
+}
+
+type Address struct {
+	Iden    string `json:"device_iden"`
+	Type    string `json:"type"`
+	Name    string `json:"name"`
+	Address string `json:"address"`
 }
 
 // PushAddress pushes a geo address with name and address to a specific PushBullet device.
-func (c *Client) PushAddress(deviceId int, name, address string) error {
-	data := url.Values{
-		"type":    {"address"},
-		"name":    {name},
-		"address": {address},
+func (c *Client) PushAddress(iden string, name, address string) error {
+	data := Address{
+		Iden:    iden,
+		Type:    "address",
+		Name:    name,
+		Address: address,
 	}
-	return c.Push(deviceId, data)
+	return c.Push(data)
+}
+
+type List struct {
+	Iden  string   `json:"device_iden"`
+	Type  string   `json:"type"`
+	Title string   `json:"title"`
+	Items []string `json:"items"`
 }
 
 // PushList pushes a list with name and a slice of items to a specific PushBullet device.
-func (c *Client) PushList(deviceId int, title string, items []string) error {
-	data := url.Values{
-		"type":  {"list"},
-		"title": {title},
-		"items": items,
+func (c *Client) PushList(iden string, title string, items []string) error {
+	data := List{
+		Iden:  iden,
+		Type:  "list",
+		Title: title,
+		Items: items,
 	}
-	return c.Push(deviceId, data)
+	return c.Push(data)
+}
+
+type Link struct {
+	Iden  string `json:"device_iden"`
+	Type  string `json:"type"`
+	Title string `json:"title"`
+	URL   string `json:"url"`
+	Body  string `json:"body,omitempty"`
 }
 
 // PushLink pushes a link with a title and url to a specific PushBullet device.
-func (c *Client) PushLink(deviceId int, title, u string) error {
-	data := url.Values{
-		"type":  {"link"},
-		"title": {title},
-		"url":   {u},
+func (c *Client) PushLink(iden, title, u, body string) error {
+	data := Link{
+		Iden:  iden,
+		Type:  "link",
+		Title: title,
+		URL:   u,
+		Body:  body,
 	}
-	return c.Push(deviceId, data)
+	return c.Push(data)
 }
