df6962f237ecc6fc15d3cb68f76a7f90607f40aa
James Durand <james.durand@alumni.msoe.edu>
2019-10-14 19:54:53 -0500

    Added context variations of API methods

--- pushbullet_test.go
+++ pushbullet_test.go
@@ -2,6 +2,7 @@ package pushbullet
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -10,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var d = &Device{
+var mockDevice = &Device{
 	Active:            true,
 	AppVersion:        8623,
 	Created:           1.412047948579029e+09,
@@ -23,19 +24,19 @@ var d = &Device{
 	PushToken:         "production:f73be0ee7877c8c7fa69b1468cde764f",
 }
 
-var n = &Note{
+var mockNote = &Note{
 	Type:  "note",
 	Title: "Space Travel Ideas",
 	Body:  "Space Elevator, Mars Hyperloop, Space Model S (Model Space?)",
 }
 
-var e = &ErrResponse{
+var mockError = &ErrResponse{
 	Type:    "invalid_request",
 	Message: "The resource could not be found.",
 	Cat:     "~(=^‥^)",
 }
 
-var m = &User{
+var mockUser = &User{
 	Created:         1.381092887398433e+09,
 	Email:           "elon@teslamotors.com",
 	EmailNormalized: "elon@teslamotors.com",
@@ -45,14 +46,14 @@ var m = &User{
 	Name:            "Elon Musk",
 }
 
-var l = &Link{
+var mockLink = &Link{
 	Type:  "link",
 	Title: "Google",
 	Body:  "Google homepage",
 	URL:   "https://www.google.com",
 }
 
-var s = &EphemeralPush{
+var mockSMS = &EphemeralPush{
 	Type:             "messaging_extension_reply",
 	PackageName:      "com.pushbullet.android",
 	SourceUserIden:   "ujpah72o0",
@@ -61,7 +62,7 @@ var s = &EphemeralPush{
 	Message:          "Hello!",
 }
 
-var c = &Channel{
+var mockChannel = &Channel{
 	Iden:        "ujxPklLhvyKsjAvkMyTVh6",
 	Tag:         "elonmusknews",
 	Name:        "Elon Musk News",
@@ -70,34 +71,34 @@ var c = &Channel{
 	WebsiteURL:  "http://elonmuscknews.com",
 }
 
-var sub = &Subscription{
+var mockSubscription = &Subscription{
 	Active:   true,
-	Channel:  c,
+	Channel:  mockChannel,
 	Created:  1.412047948579029e+09,
 	Iden:     "ujpah72o0sjAoRtnM0jc",
 	Modified: 1.412047948579031e+09,
 }
 
-var k = "API_KEY"
+var apiKey = "API_KEY"
 
 func PushbulletResponseStub() *httptest.Server {
 	var resp string
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch r.RequestURI {
 		case "/devices":
-			d, _ := json.Marshal(d)
+			d, _ := json.Marshal(mockDevice)
 			resp = `{ "devices": [` + string(d) + `] }`
 		case "/users/me":
-			m, _ := json.Marshal(m)
+			m, _ := json.Marshal(mockUser)
 			resp = string(m)
 		case "/pushes":
-			n, _ := json.Marshal(n)
+			n, _ := json.Marshal(mockNote)
 			resp = string(n)
 		case "/ephemerals":
-			s, _ := json.Marshal(s)
+			s, _ := json.Marshal(mockSMS)
 			resp = string(s)
 		case "/subscriptions":
-			sub, _ := json.Marshal(sub)
+			sub, _ := json.Marshal(mockSubscription)
 			resp = `{ "subscriptions": [` + string(sub) + `] }`
 		default:
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -110,7 +111,7 @@ func PushbulletResponseStub() *httptest.Server {
 
 func PushbulletErrJSONResponseStub() *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		e, _ := json.Marshal(e)
+		e, _ := json.Marshal(mockError)
 		resp := `{ "error":` + string(e) + `}`
 		w.Header().Set("Content-Type", "application/json")
 		http.Error(w, resp, http.StatusInternalServerError)
@@ -124,58 +125,58 @@ func PushbulletErrResponseStub() *httptest.Server {
 }
 
 func TestNew(t *testing.T) {
-	pb := New(k)
-	assert.Equal(t, k, pb.Key)
+	pb := New(apiKey)
+	assert.Equal(t, apiKey, pb.Key)
 }
 
 func TestNewWithClient(t *testing.T) {
 	c := &http.Client{}
-	pb := NewWithClient(k, c)
+	pb := NewWithClient(apiKey, c)
 	assert.Equal(t, c, pb.Client)
 }
 
 func TestError(t *testing.T) {
-	assert.Equal(t, e.Message, e.Error())
+	assert.Equal(t, mockError.Message, mockError.Error())
 }
 
 func TestBuildRequest(t *testing.T) {
-	pb := New(k)
-	req := pb.buildRequest("/pushes", n)
+	pb := New(apiKey)
+	req := pb.buildRequest(context.Background(), "/pushes", mockNote)
 	buf := new(bytes.Buffer)
 	_, _ = buf.ReadFrom(req.Body)
 	var note Note
 	_ = json.Unmarshal(buf.Bytes(), &note)
 	assert.Equal(t, "POST", req.Method)
-	assert.Equal(t, n, &note)
+	assert.Equal(t, mockNote, &note)
 }
 
 func TestDevices(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	d.Client = pb
+	mockDevice.Client = pb
 	devs, err := pb.Devices()
 	assert.NoError(t, err)
 	assert.Len(t, devs, 1)
-	assert.Equal(t, d, devs[0])
+	assert.Equal(t, mockDevice, devs[0])
 }
 
 func TestDeviceWithNickname(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	dev, err := pb.Device(d.Nickname)
+	dev, err := pb.Device(mockDevice.Nickname)
 	assert.NoError(t, err)
-	assert.Equal(t, d.Nickname, dev.Nickname)
+	assert.Equal(t, mockDevice.Nickname, dev.Nickname)
 	assert.Equal(t, pb, dev.Client)
 }
 
 func TestDeviceWithNicknameMissing(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
 	dev, err := pb.Device("MISSING")
 	assert.Error(t, err)
@@ -185,37 +186,37 @@ func TestDeviceWithNicknameMissing(t *testing.T) {
 func TestDevicePushNote(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	dev, _ := pb.Device(d.Nickname)
-	err := dev.PushNote(n.Title, n.Body)
+	dev, _ := pb.Device(mockDevice.Nickname)
+	err := dev.PushNote(mockNote.Title, mockNote.Body)
 	assert.NoError(t, err)
 }
 
 func TestDevicePushLink(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	dev, _ := pb.Device(d.Nickname)
-	err := dev.PushLink(l.Title, l.URL, l.Body)
+	dev, _ := pb.Device(mockDevice.Nickname)
+	err := dev.PushLink(mockLink.Title, mockLink.URL, mockLink.Body)
 	assert.NoError(t, err)
 }
 
 func TestDevicePushSMS(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	dev, _ := pb.Device(d.Nickname)
-	err := dev.PushSMS(s.TargetDeviceIden, s.ConversationIden, s.Message)
+	dev, _ := pb.Device(mockDevice.Nickname)
+	err := dev.PushSMS(mockSMS.TargetDeviceIden, mockSMS.ConversationIden, mockSMS.Message)
 	assert.NoError(t, err)
 }
 
 func TestDevicesError(t *testing.T) {
 	server := PushbulletErrResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
 	devs, err := pb.Devices()
 	assert.Error(t, err)
@@ -226,28 +227,28 @@ func TestDevicesError(t *testing.T) {
 func TestDevicesJSONError(t *testing.T) {
 	server := PushbulletErrJSONResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
 	devs, err := pb.Devices()
 	assert.Error(t, err)
 	assert.Len(t, devs, 0)
-	assert.Equal(t, e, err)
+	assert.Equal(t, mockError, err)
 }
 
 func TestMe(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
 	me, err := pb.Me()
 	assert.NoError(t, err)
-	assert.Equal(t, m, me)
+	assert.Equal(t, mockUser, me)
 }
 
 func TestMeError(t *testing.T) {
 	server := PushbulletErrResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
 	_, err := pb.Me()
 	assert.Error(t, err)
@@ -257,28 +258,28 @@ func TestMeError(t *testing.T) {
 func TestMeJSONError(t *testing.T) {
 	server := PushbulletErrJSONResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
 	_, err := pb.Me()
 	assert.Error(t, err)
-	assert.Equal(t, e, err)
+	assert.Equal(t, mockError, err)
 }
 
 func TestPush(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	err := pb.Push("/pushes", n)
+	err := pb.Push("/pushes", mockNote)
 	assert.NoError(t, err)
 }
 
 func TestPushError(t *testing.T) {
 	server := PushbulletErrResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	err := pb.Push("/pushes", n)
+	err := pb.Push("/pushes", mockNote)
 	assert.Error(t, err)
 	assert.Equal(t, "500 Internal Server Error", err.Error())
 }
@@ -286,74 +287,74 @@ func TestPushError(t *testing.T) {
 func TestPushJSONError(t *testing.T) {
 	server := PushbulletErrJSONResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	err := pb.Push("/pushes", n)
+	err := pb.Push("/pushes", mockNote)
 	assert.Error(t, err)
-	assert.Equal(t, e, err)
+	assert.Equal(t, mockError, err)
 }
 
 func TestPushLink(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	err := pb.PushLink(m.Iden, l.Title, l.URL, l.Body)
+	err := pb.PushLink(mockUser.Iden, mockLink.Title, mockLink.URL, mockLink.Body)
 	assert.NoError(t, err)
 }
 
 func TestPushNote(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	err := pb.PushNote(m.Iden, n.Title, n.Body)
+	err := pb.PushNote(mockUser.Iden, mockNote.Title, mockNote.Body)
 	assert.NoError(t, err)
 }
 
 func TestPushLinkToChannel(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	err := pb.PushLinkToChannel(sub.Channel.Tag, l.Title, l.URL, l.Body)
+	err := pb.PushLinkToChannel(mockSubscription.Channel.Tag, mockLink.Title, mockLink.URL, mockLink.Body)
 	assert.NoError(t, err)
 }
 
 func TestPushNoteToChannel(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	err := pb.PushNoteToChannel(sub.Channel.Tag, n.Title, n.Body)
+	err := pb.PushNoteToChannel(mockSubscription.Channel.Tag, mockNote.Title, mockNote.Body)
 	assert.NoError(t, err)
 }
 
 func TestPushSMS(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	err := pb.PushSMS(s.SourceUserIden, s.TargetDeviceIden, s.ConversationIden, s.Message)
+	err := pb.PushSMS(mockSMS.SourceUserIden, mockSMS.TargetDeviceIden, mockSMS.ConversationIden, mockSMS.Message)
 	assert.NoError(t, err)
 }
 
 func TestSubscriptions(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	sub.Client = pb
+	mockSubscription.Client = pb
 	subs, err := pb.Subscriptions()
 	assert.NoError(t, err)
 	assert.Len(t, subs, 1)
-	assert.Equal(t, sub, subs[0])
+	assert.Equal(t, mockSubscription, subs[0])
 }
 
 func TestSubscriptionsError(t *testing.T) {
 	server := PushbulletErrResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
 	subs, err := pb.Subscriptions()
 	assert.Error(t, err)
@@ -364,29 +365,29 @@ func TestSubscriptionsError(t *testing.T) {
 func TestSubscriptionsJSONError(t *testing.T) {
 	server := PushbulletErrJSONResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
 	subs, err := pb.Subscriptions()
 	assert.Error(t, err)
 	assert.Len(t, subs, 0)
-	assert.Equal(t, e, err)
+	assert.Equal(t, mockError, err)
 }
 
 func TestSubscriptionWithName(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	sub, err := pb.Subscription(c.Tag)
+	sub, err := pb.Subscription(mockChannel.Tag)
 	assert.NoError(t, err)
-	assert.Equal(t, c.Tag, sub.Channel.Tag)
+	assert.Equal(t, mockChannel.Tag, sub.Channel.Tag)
 	assert.Equal(t, pb, sub.Client)
 }
 
 func TestSubscriptionWithNameMissing(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
 	sub, err := pb.Subscription("MISSING")
 	assert.Error(t, err)
@@ -396,19 +397,19 @@ func TestSubscriptionWithNameMissing(t *testing.T) {
 func TestSubscriptionPushNote(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	sub, _ := pb.Subscription(sub.Channel.Tag)
-	err := sub.PushNote(n.Title, n.Body)
+	sub, _ := pb.Subscription(mockSubscription.Channel.Tag)
+	err := sub.PushNote(mockNote.Title, mockNote.Body)
 	assert.NoError(t, err)
 }
 
 func TestSubscriptionPushLink(t *testing.T) {
 	server := PushbulletResponseStub()
 	defer server.Close()
-	pb := New(k)
+	pb := New(apiKey)
 	pb.Endpoint.URL = server.URL
-	sub, _ := pb.Subscription(sub.Channel.Tag)
-	err := sub.PushLink(l.Title, l.URL, l.Body)
+	sub, _ := pb.Subscription(mockSubscription.Channel.Tag)
+	err := sub.PushLink(mockLink.Title, mockLink.URL, mockLink.Body)
 	assert.NoError(t, err)
 }
